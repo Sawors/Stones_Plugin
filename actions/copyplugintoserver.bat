9fae003c39b4bfb0d1c05c4d3494801bd510b604
Sawors <40705096+Sawors@users.noreply.github.com>
2022-08-31 19:42:06 +0200

    Delete actions directory

--- actions/copyplugintoserver.bat
+++
@@ -1,2 +0,0 @@
-del /q "C:\Users\sosol\IdeaProjects\StonesPLG\target\Stones-*-shaded.jar"
-xcopy "C:\Users\sosol\IdeaProjects\StonesPLG\target\Stones-*.jar" "C:\Users\sosol\IdeaProjects\_SERVERS\Stones\plugins" /y /s
